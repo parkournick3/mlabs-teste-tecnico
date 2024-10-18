4cc35987dbb1560f8338f906772d16fa461e9193
Nicolas <nicolasalexandre0001@gmail.com>
2024-10-18 06:53:52 -0300

    chore: generate parking controller, module and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ParkingModule } from './parking/parking.module';
 
 const DATABASE_URL =
   process.env.DATABASE_URL || 'mongodb://root:password@localhost:27017/parking';
 
 @Module({
-  imports: [MongooseModule.forRoot(DATABASE_URL)],
+  imports: [MongooseModule.forRoot(DATABASE_URL), ParkingModule],
   controllers: [AppController],
   providers: [AppService],
 })
