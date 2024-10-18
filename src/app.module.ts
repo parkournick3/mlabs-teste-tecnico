2f7e5b2e08e207677a4fdb3a153655b3aeb1731f
Nicolas <nicolasalexandre0001@gmail.com>
2024-10-18 06:51:21 -0300

    chore: configure database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
+const DATABASE_URL =
+  process.env.DATABASE_URL || 'mongodb://root:password@localhost:27017/parking';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(DATABASE_URL)],
   controllers: [AppController],
   providers: [AppService],
 })
