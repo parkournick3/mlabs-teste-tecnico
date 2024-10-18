d2ada33e4aab4ed1108fda3b0fd7aec1995d496a
Nicolas <nicolasalexandre0001@gmail.com>
2024-10-18 08:43:14 -0300

    fix: db connection uri

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,8 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ParkingModule } from './parking/parking.module';
 
 const DATABASE_URL =
-  process.env.DATABASE_URL || 'mongodb://root:password@localhost:27017/parking';
+  process.env.DATABASE_URL ||
+  'mongodb://root:password@localhost:27017/parking?authSource=admin';
 
 @Module({
   imports: [MongooseModule.forRoot(DATABASE_URL), ParkingModule],
